b0e099ebbf761611316e3533114915d73effc695
Grajek543 <gfiejtek@gmail.com>
2024-12-05 09:41:21 +0100

    Dodanie klasy oraz fstream pliku mac

---
+++ zaawansowane4/zaawansowane4/class.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <iostream>
+#include <ostream>
+using namespace std;
+
+class matrix {
+private:
+
+public:
+	matrix(void);
+	matrix(int n);
+	matrix(int n, int* t);
+	matrix(matrix& m);
+	~matrix(void);
+	matrix& alokuj(int n);
+	matrix& wstaw(int x, int y, int wartosc);
+	int pokaz(int x, int y);
+	matrix& dowroc(void);
+	matrix& losuj(void);
+	matrix& losuj(int x);
+	matrix& diagonalna(int* t);
+	matrix& diagonalna_k(int k, int* t);
+	matrix& kolumna(int x, int* t);
+	matrix& wiersz(int y, int* t);
+	matrix& przekatna(void);
+	matrix& pod_przekatna(void);
+	matrix& nad_przekatna(void);
+	matrix& szachownica(void);
+	matrix& operator+(matrix& m);
+	matrix& operator*(matrix& m);
+	matrix& operator+(int a);
+	matrix& operator*(int a);
+	matrix& operator-(int a);
+	friend matrix operator+(int a, matrix& m);
+	friend matrix operator*(int a, matrix& m);
+	friend matrix operator-(int a, matrix& m);
+	matrix& operator++(int);
+	matrix& operator--(int);
+	matrix& operator+=(int a);
+	matrix& operator-=(int a);
+	matrix& operator*=(int a);
+	matrix& operator(double);
+	friend ostream& operator<<(ostream& o, matrix& m);
+	bool operator==(const matrix& m);
+	bool operator>(const matrix& m);
+	bool operator<(const matrix& m);
+};
---
+++ zaawansowane4/zaawansowane4/zaawansowane4.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    fstream mac;
+    mac.open("mac.txt", std::ios::out | std::ios::trunc);
+    if (!mac.is_open()) {
+        std::cout << "Nie ma dostepu do pliku PRE.txt" << std::endl;
+        return;
+    }
+
+
+    mac.close();
+}
